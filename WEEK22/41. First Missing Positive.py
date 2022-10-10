2ea35013c4fdff9908b927c6f6e7eb8e988da3b4
sok891111 <sok891111@gmail.com>
2022-10-10 23:41:58 +0900

    Update 41. First Missing Positive.py

--- WEEK22/41. First Missing Positive.py
+++ WEEK22/41. First Missing Positive.py
@@ -1,12 +1,5 @@
 class Solution(object):
     def firstMissingPositive(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        lmax = max(nums)
-        if lmax < 0 :
-            return 1
         m = [False]*(len(nums)+1)
         m[0] = True
         for num in nums:
@@ -17,4 +10,4 @@ class Solution(object):
             if visited == False:
                 return index
             
-        return lmax+1
+        return max(nums)+1
