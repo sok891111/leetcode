e2c55bbd386a2000efc22f73c63f34ae71bbb453
sok891111 <sok891111@gmail.com>
2023-02-19 14:38:55 +0900

    Create 96. Unique Binary Search Trees.py

---
+++ 96. Unique Binary Search Trees.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [0]* (n+1)
+        dp[0] = 1
+        dp[1] = 1
+        #catalan number
+        for i in range(2,n+1):
+            for j in range(i):
+                dp[i] +=  dp[j] * dp[i-j-1]
+
+        
+        return dp[-1]
